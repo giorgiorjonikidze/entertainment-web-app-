902ddaf40c6da86a6f50b5c85827a94faa2e1e49
WINDOWS-9CI3A45\User <gioorjo123@gmail.com>
2022-11-27 16:29:25 +0400

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      <h1 className="text-[100px]">hello world!</h1>
     </div>
   );
 }
